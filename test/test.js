0d992d8f14bbd510ed1b63b86b4b41a84c91d9f8
gllms <willemvanderdoe@hotmail.com>
2019-11-21 07:53:57 +0000

    update tests to lowercase

--- test/test.js
+++ test/test.js
@@ -16,12 +16,12 @@ QUnit.test("create class without arguments", function (assert) {
 });
 
 QUnit.module("functions");
-QUnit.test("CHR$", function (assert) {
-  assert.deepEqual(t.functions.CHR$(65), "A", "CHR$(65) should give 'A'");
+QUnit.test("chr$", function (assert) {
+  assert.deepEqual(t.functions.chr$(65), "A", "chr$(65) should give 'A'");
 });
-QUnit.test("INT", function (assert) {
-  assert.deepEqual(t.functions.INT(32.8), 32, "INT(32.8) should give 32");
+QUnit.test("int", function (assert) {
+  assert.deepEqual(t.functions.int(32.8), 32, "int(32.8) should give 32");
 });
-QUnit.test("INUM", function (assert) {
-  assert.deepEqual(t.functions.INUM(32.8), 33, "INUM(32.8) should give 33");
+QUnit.test("inum", function (assert) {
+  assert.deepEqual(t.functions.inum(32.8), 33, "inum(32.8) should give 33");
 });
