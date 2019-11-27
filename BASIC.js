11783e36c825f82804f55ae8fbb5faecca429d1e
gllms <willemvanderdoe@hotmail.com>
2019-11-27 20:51:49 +0100

    Add newline character

--- BASIC.js
+++ BASIC.js
@@ -464,27 +464,33 @@ class SyntaxTree {
 
   draw(chars, user) {
     chars.split("").forEach((char) => {
-      let bin = this.parseHex(this.chars[char.charCodeAt(0)]);
-      const ctx = this.canvas.getContext("2d");
-      bin.forEach((e, i) => {
-        e = e.split("");
-        let colour = user ? "white" : "cyan";
-        let c = e.splice(0, 2);
-        if (c[0] == 0 && c[1] == 0) colour = user ? "red" : "black";
-        else if (c[0] == 0 && c[1] == 1) colour = user ? "magenta" : "blue";
-        else if (c[0] == 1 && c[1] == 0) colour = user ? "yellow" : "green";
-        ctx.fillStyle = colour;
-        e.forEach((b, j) => {
-          if (parseInt(b)) ctx.fillRect(this.cpos.x * 6 + j, this.cpos.y * 9 + i, 1, 1);
+      let code = char.charCodeAt(0);
+      if (code == 10) this.newline();
+      else {
+        let bin = this.parseHex(this.chars[code]);
+        const ctx = this.canvas.getContext("2d");
+        bin.forEach((e, i) => {
+          e = e.split("");
+          let colour = user ? "white" : "cyan";
+          let c = e.splice(0, 2);
+          if (c[0] == 0 && c[1] == 0) colour = user ? "red" : "black";
+          else if (c[0] == 0 && c[1] == 1) colour = user ? "magenta" : "blue";
+          else if (c[0] == 1 && c[1] == 0) colour = user ? "yellow" : "green";
+          ctx.fillStyle = colour;
+          e.forEach((b, j) => {
+            if (parseInt(b)) ctx.fillRect(this.cpos.x * 6 + j, this.cpos.y * 9 + i, 1, 1);
+          });
         });
-      });
-      if (this.cpos.x > 38) {
-        this.cpos.x = 0;
-        this.cpos.y++;
-      } else this.cpos.x++;
+        if (this.cpos.x > 38) this.newline();
+        else this.cpos.x++;
+      }
     });
+    this.newline();
+  }
+
+  newline() {
     this.cpos.x = 0;
-    this.cpos.y++;
+    if (this.cpos.y < 23) this.cpos.y++
   }
 
   reset() {
